44eee87a1003f524672f242cae16883e8fec6ec9
resccu <fawnliu2333@gmail.com>
2017-07-17 11:42:24 +0800

    add readme

--- KNN/knn.py
+++ KNN/knn.py
@@ -7,19 +7,18 @@ def createDataSet():
 
 def classify0(inx,dataSet,labels,k):
     dataSetSize = dataSet.shape[0]
-
-    diffMat = tile(inx,(dataSetSize,1)) - dataSet
-    sqDiffMat = diffMat ** 2
-    sqDistances = sqDiffMat.sum(axis=1)
+    diffMat = dataSet - tile(inx, (dataSetSize, 1))
+    sqDiffmat = diffMat ** 2
+    sqDistances = sqDiffmat.sum(axis=1)
     distance = sqDistances ** 0.5
     sortedDistIndecies = distance.argsort()
     classCount = {}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndecies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+        voteIlabes = labels[sortedDistIndecies[i]]
+        classCount[voteIlabes] = classCount.get(voteIlabes, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
 group, labels = createDataSet()
-result = classify0([0,0],group,labels,3)
+result = classify0([0, 0], group, labels, 3)
 print result
