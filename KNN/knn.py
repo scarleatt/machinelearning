194c8d72c30f9e9e4133c746ed984e1151f8ee3a
resccu <fawnliu2333@gmail.com>
2017-07-17 17:00:13 +0800

    test

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,11 +1,21 @@
 from numpy import *;
 import operator
-def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    lables = ['A','A','B','B']
-    return group,lables
 
-def classify0(inx,dataSet,labels,k):
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    returnMat = zeros((len(arrayOLines), 3))
+    classLabelvector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelvector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelvector
+
+def classify0(inx, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = dataSet - tile(inx, (dataSetSize, 1))
     sqDiffmat = diffMat ** 2
@@ -19,6 +29,34 @@ def classify0(inx,dataSet,labels,k):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-group, labels = createDataSet()
-result = classify0([0, 0], group, labels, 3)
-print result
+# plot
+# import matplotlib.pyplot as plt
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(returnMat[:, 0], returnMat[:, 1], 15.0*array(classLabelvector), 15.0*array(classLabelvector))
+# plt.show()
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = (dataSet-tile(minVals, (m, 1)))/(tile(ranges, (m, 1)))
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    ratio = 0.1
+    dataMat, datalabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(dataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*ratio)
+    errCount = 0
+    for i in range(numTestVecs):
+        result = classify0(normMat[i,:], normMat[numTestVecs:m,:], datalabels[numTestVecs:m],3)
+        print "The classifier came back with: %d, the real answer is %d" \
+              % (result, datalabels[i])
+        if (result != datalabels[i]): errCount += 1
+    print "The total error rate is %f " % (errCount / float(numTestVecs))
+
+datingClassTest()
--- KNN/readme.md
+++ KNN/readme.md
@@ -1,6 +1,8 @@
 + tile
   - 复制矩阵
 +  argsort
-    + 返回数组内值从小到大的索引值
+    + 返回数组内值从小到大的索引值  
++ 数据归一化处理
+    ``newValue = (oldValue-min)/(max-min)``
 
 
