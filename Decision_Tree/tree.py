474e8b83225e3f46361ccdaaf72652ab8d23e2c1
resccu <fawnliu2333@gmail.com>
2017-07-19 15:28:09 +0800

    mail

--- Decision_Tree/tree.py
+++ Decision_Tree/tree.py
@@ -26,87 +26,90 @@ def createDataSet():
     labels = ['no surfacing', 'flippers']
     return dataSet, labels
 
-def splitDataSet(dataSet, axis, value):
-    retDataSet = []
-    for featVec in dataSet:
-        if featVec[axis] == value:
-            reducedFeatvec = featVec[:axis]
-            reducedFeatvec.extend(featVec[axis+1:])
-            retDataSet.append(reducedFeatvec)
-    return retDataSet
+dataSet, labels = createDataSet()
+calcShannoEnt(dataSet)
 
-def chooseBestFeatureToSplit(dataSet):
-    numFeatures = len(dataSet[0]) - 1
-    baseEntropy = calcShannoEnt(dataSet)
-    bestInfoGain = 0.0; bestFeature = -1
-    for i in range(numFeatures):
-        featList = [example[i] for example in dataSet]
-        uniquevals = set(featList)
-        newEntropy = 0
-        for value in uniquevals:
-            subDataSet = splitDataSet(dataSet, i, value)
-            prob = len(subDataSet)/float(len(dataSet))
-            newEntropy += prob*calcShannoEnt(subDataSet)
-        infoGain = baseEntropy - newEntropy
-        if (infoGain > bestInfoGain):
-            bestInfoGain = infoGain
-            bestFeature = i
-    return bestFeature
-
-def majorityCnt(classList):
-    classCount = {}
-    for vote in classList:
-        if vote in classCount.keys():
-            classCount[vote] = 0
-        classCount[vote] += 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    print sortedClassCount
-    return sortedClassCount[0][0]
-
-def createTree(dataSet, labels):
-    classList = [example[-1] for example in dataSet]
-    if classList.count(classList[0]) == len(classList):
-        return classList[0]
-    if len(dataSet[0]) == 1:
-        return majorityCnt(classList)
-    bestFeat = chooseBestFeatureToSplit(dataSet)
-    bestFeatLabel = labels[bestFeat]
-    myTree = {bestFeatLabel: {}}
-    del(labels[bestFeat])
-    featValues = [example[bestFeat] for example in dataSet]
-    uniqueVals = set(featValues)
-    for value in uniqueVals:
-        subLabels = labels[:]
-        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
-    return myTree
-
-def classify(inputTree, featlabels, testVec):
-    firstStr = inputTree.keys()[0]
-    secondDict = inputTree[firstStr]
-    featIndex = featlabels.index(firstStr)
-    print featlabels, featIndex
-    for key in secondDict.keys():
-        print key
-        if testVec[featIndex] == key:
-            if type(secondDict[key]).__name__ == 'dict':
-                classLabel = classify(secondDict[key], featlabels, testVec)
-            else: classLabel = secondDict[key]
-    return classLabel
-
-def storeTree(inputTree, filename):
-    import pickle
-    fw = open(filename, 'w')
-    pickle.dump(inputTree, fw)
-    fw.close()
-
-def grapTree(filename):
-    import pickle
-    fr = open(filename)
-    return pickle.load(fr)
-
-fr = open('lenses.txt')
-labels = ['age', 'prescript', 'astigmatic', 'tearRate']
-lenses = [inst.strip().split('\t') for inst in fr.readlines()]
-print lenses
-myTree = createTree(lenses, labels)
-print myTree
+# def splitDataSet(dataSet, axis, value):
+#     retDataSet = []
+#     for featVec in dataSet:
+#         if featVec[axis] == value:
+#             reducedFeatvec = featVec[:axis]
+#             reducedFeatvec.extend(featVec[axis+1:])
+#             retDataSet.append(reducedFeatvec)
+#     return retDataSet
+#
+# def chooseBestFeatureToSplit(dataSet):
+#     numFeatures = len(dataSet[0]) - 1
+#     baseEntropy = calcShannoEnt(dataSet)
+#     bestInfoGain = 0.0; bestFeature = -1
+#     for i in range(numFeatures):
+#         featList = [example[i] for example in dataSet]
+#         uniquevals = set(featList)
+#         newEntropy = 0
+#         for value in uniquevals:
+#             subDataSet = splitDataSet(dataSet, i, value)
+#             prob = len(subDataSet)/float(len(dataSet))
+#             newEntropy += prob*calcShannoEnt(subDataSet)
+#         infoGain = baseEntropy - newEntropy
+#         if (infoGain > bestInfoGain):
+#             bestInfoGain = infoGain
+#             bestFeature = i
+#     return bestFeature
+#
+# def majorityCnt(classList):
+#     classCount = {}
+#     for vote in classList:
+#         if vote in classCount.keys():
+#             classCount[vote] = 0
+#         classCount[vote] += 1
+#     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+#     print sortedClassCount
+#     return sortedClassCount[0][0]
+#
+# def createTree(dataSet, labels):
+#     classList = [example[-1] for example in dataSet]
+#     if classList.count(classList[0]) == len(classList):
+#         return classList[0]
+#     if len(dataSet[0]) == 1:
+#         return majorityCnt(classList)
+#     bestFeat = chooseBestFeatureToSplit(dataSet)
+#     bestFeatLabel = labels[bestFeat]
+#     myTree = {bestFeatLabel: {}}
+#     del(labels[bestFeat])
+#     featValues = [example[bestFeat] for example in dataSet]
+#     uniqueVals = set(featValues)
+#     for value in uniqueVals:
+#         subLabels = labels[:]
+#         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+#     return myTree
+#
+# def classify(inputTree, featlabels, testVec):
+#     firstStr = inputTree.keys()[0]
+#     secondDict = inputTree[firstStr]
+#     featIndex = featlabels.index(firstStr)
+#     print featlabels, featIndex
+#     for key in secondDict.keys():
+#         print key
+#         if testVec[featIndex] == key:
+#             if type(secondDict[key]).__name__ == 'dict':
+#                 classLabel = classify(secondDict[key], featlabels, testVec)
+#             else: classLabel = secondDict[key]
+#     return classLabel
+#
+# def storeTree(inputTree, filename):
+#     import pickle
+#     fw = open(filename, 'w')
+#     pickle.dump(inputTree, fw)
+#     fw.close()
+#
+# def grapTree(filename):
+#     import pickle
+#     fr = open(filename)
+#     return pickle.load(fr)
+#
+# fr = open('lenses.txt')
+# labels = ['age', 'prescript', 'astigmatic', 'tearRate']
+# lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+# print lenses
+# myTree = createTree(lenses, labels)
+# print myTree
