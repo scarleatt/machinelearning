fccedf8e69ce6e96db8b0aaa96d0488cd39610ac
resccu <fawnliu2333@gmail.com>
2017-07-19 18:47:14 +0800

    change

--- Bayes/bayes.py
+++ Bayes/bayes.py
@@ -19,7 +19,7 @@ def createVocabList(dataSet):
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
-        if word in vocabList:
+        if word in inputSet:
             returnVec[vocabList.index(word)] = 1
         else: print "the word: %s is not in my Vocabulary!" % word
     return returnVec
@@ -27,8 +27,9 @@ def setOfWords2Vec(vocabList, inputSet):
 def bagOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
     for word in inputSet:
-        if word in vocabList:
+        if word in inputSet:
             returnVec[vocabList.index(word)] += 1
+        else: print "the word: %s is not in my Vocabulary!" % word
     return returnVec
 
 def trainNB0(trainMatrix, trainCategory):
@@ -115,63 +116,3 @@ def calcMostFreq(vocabList, fullText):
     sortedFreq = sorted(freqDict.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedFreq[0:30]
 
-def localWords(feed1, feed0):
-    docList = []; classList = []; fullText = []
-    minLen = min(len(feed1['entries']), len(feed0['entries']))
-    for i in range(minLen):
-        wordList = textParse(feed1['entries'][i]['summary'])
-        docList.append(wordList)
-        fullText.extend(wordList)
-        classList.append(1)
-        wordList = textParse(feed0['entries'][i]['summary'])
-        docList.append(wordList)
-        fullText.extend(wordList)
-        classList.append(0)
-    vocabList = createVocabList(docList)
-    top30Words = calcMostFreq(vocabList, fullText)
-    for pairW in top30Words:
-        if pairW[0] in vocabList: vocabList.remove(pairW[0])
-    trainingSet = range(2*minLen); testSet = []
-    for i in range(20):
-        randIndex = int(random.uniform(0, len(trainingSet)))
-        testSet.append(trainingSet[randIndex])
-        del(trainingSet[randIndex])
-    trainMat = []; trainClasses = []
-    for docIndex in trainingSet:
-        trainMat.append(bagOfWords2Vec(vocabList, docList[docIndex]))
-        trainClasses.append(classList[docIndex])
-    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
-    errCount = 0
-    for docIndex in testSet:
-        wordVector = bagOfWords2Vec(vocabList, docList[docIndex])
-        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
-            errCount += 1
-    print 'the error rate is: ', float(errCount) / len(testSet)
-    return vocabList, p0V, p1V
-
-def getTopWords(ny, sf):
-    import operator
-    vocabList, p0V, p1V = localWords(ny, sf)
-    topNY = []; topSF = []
-    for i in range(len(p0V)):
-        if p0V[i] > -6.0: topSF.append((vocabList[i], p0V[i]))
-        if p1V[i] > -6.0: topNY.append((vocabList[i], p1V[i]))
-    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
-    print "SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF"
-    for item in sortedSF:
-        print item[0]
-    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
-    print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY"
-    for item in sortedNY:
-        print item[0]
-
-# import feedparser
-# ny = feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
-# sf = feedparser.parse('http://sfbay.craigslist.org/stp/index.rss')
-# getTopWords(ny, sf)
-# localWords(ny, sf)
-
-# del counts = 30
-    # error rate = 0.4
-# del counts = 50
-    # error rate = 0.15
