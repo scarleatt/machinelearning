97bb2946308d9ce186fa2e1e33bbed21f628da2e
resccu <fawnliu2333@gmail.com>
2017-07-20 13:14:47 +0800

    logistic finished

--- Logistic/logRegres.py
+++ Logistic/logRegres.py
@@ -58,23 +58,52 @@ def stocGradAscent0(dataMat, classLabels):
         weights = weights + alpha * error * dataMat[i]
     return weights
 
-def stocGradAxcent1(dataMat, classLabels, numIter=150):
+def stocGradAscent1(dataMat, classLabels, numIter=150):
     m, n = shape(dataMat)
     weights = ones(n)
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
-            alpha = 1/(1.0+j+i)+0.01
-            randIndex = int(random.uniform(0, len(dataIndex)))
+            alpha = 1/(1.0+j+i)+0.01   # change alpha
+            randIndex = int(random.uniform(0, len(dataIndex)))  # randomly selected updates
             h = sigmoid(sum(dataMat[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMat[randIndex]
             del(dataIndex[randIndex])
     return weights
 
-dataMat, labelMat = loadDataSet()
-print gradAscent(dataMat, labelMat)
-print stocGradAscent0(array(dataMat), labelMat)
-weights = stocGradAxcent1(array(dataMat), labelMat, 500)
-print weights
-plotBestFit(weights)
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX*weights))
+    if prob > 0.5: return 1
+    else: return 0
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 500)
+    errCount = 0; numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+            errCount += 1
+    errorRate = (float(errCount)/numTestVec)
+    print "the error rate of this test is: %f" % errorRate
+    return errorRate
+
+def multiTest():
+    numTest = 10; errorSum = 0
+    for k in range(numTest):
+        errorSum += colicTest()
+    print "after %d iterations the average error rate is: %f" % (numTest, errorSum/float(numTest))
