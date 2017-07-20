f689788e94b16c525b89a07ab5babedd5727675d
resccu <fawnliu2333@gmail.com>
2017-07-20 10:42:16 +0800

    change

---
+++ Logistic/logRegres.py
@@ -0,0 +1,81 @@
+from numpy import *
+
+def loadDataSet():
+    dataMat = []; labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+def sigmoid(inX):
+    return 1.0 / (1 + exp(-inX))
+
+def gradAscent(dataMatIn, classLabels):
+    dataMat = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m, n = shape(dataMat)
+    alpha = 0.001
+    maxCycles = 500
+    weights = ones((n, 1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMat * weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMat.transpose() * error
+    return weights
+
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1]); ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1]); ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1'); plt.ylabel('X2')
+    plt.show()
+
+# random gradient
+def stocGradAscent0(dataMat, classLabels):
+    m, n = shape(dataMat)
+    alpha = 0.01
+    weights = ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMat[i] * weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error * dataMat[i]
+    return weights
+
+def stocGradAxcent1(dataMat, classLabels, numIter=150):
+    m, n = shape(dataMat)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 1/(1.0+j+i)+0.01
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMat[randIndex]*weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMat[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+
+dataMat, labelMat = loadDataSet()
+print gradAscent(dataMat, labelMat)
+print stocGradAscent0(array(dataMat), labelMat)
+weights = stocGradAxcent1(array(dataMat), labelMat)
+print weights
+plotBestFit(weights)
