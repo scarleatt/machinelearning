3438d05cbb516293f30f61492a7b669b76d7015a
resccu <fawnliu2333@gmail.com>
2017-07-21 10:37:30 +0800

    svm a little

---
+++ SVM/svmMLiA.py
@@ -0,0 +1,26 @@
+from numpy import *
+
+def loadDataSet(filename):
+    dataMat = []; labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
+
+def selectJrand(i, m):
+    j = i
+    while (j==i):
+        j = int(random.uniform(0, m))
+    return h
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+dataArr, labelArr = loadDataSet('testSet.txt')
+print array(labelArr)
