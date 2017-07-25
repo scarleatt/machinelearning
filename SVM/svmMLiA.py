02606279a30c8db565951cea585c5994f6fe4d2d
resccu <fawnliu2333@gmail.com>
2017-07-25 11:57:29 +0800

    fix smoP

--- SVM/svmMLiA.py
+++ SVM/svmMLiA.py
@@ -213,34 +213,28 @@ class optStruct:
             self.K[:, i] = kernelTrans(self.X, self.X[i, :], kTup)
 
 def testRbf(k1=1.3):
-    dataArr, labelArr = loadDataSet('testSetRBF.txt')
-    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf, k1'))
-    dataMat = mat(dataArr); labelMat = mat(labelArr).transpose()
-    svInd = nonzero(alphas.A > 0)
-    sVs = dataMat[svInd]
-    labelSV = labelMat[svInd]
-    print "there are %d Support Vector" % shape(sVs)[0]
-    m, n = shape(dataMat)
-    errCount = 0
+    dataArr,labelArr = loadDataSet('testSetRBF.txt')
+    b,alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1)) #C=200 important
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    svInd=nonzero(alphas.A>0)[0]
+    sVs=datMat[svInd] #get matrix of only support vectors
+    labelSV = labelMat[svInd];
+    print "there are %d Support Vectors" % shape(sVs)[0]
+    m,n = shape(datMat)
+    errorCount = 0
     for i in range(m):
-        kernelEval = kernelTrans(sVs, dataMat[i, :], ('rbf', k1))
-        predict = kernelEval.T * multiply(labelSV, alphas[svInd] + b)
-        if sign(predict) != sign(labelArr[i]): errCount += 1
-    print "the training error rate is: %f" % (float(errCount)/m)
-    dataArr, labelArr = loadDataSet('testSetRBF2.txt')
-    errCount = 0
-    dataMat = mat(dataArr); labelMat = mat(labelArr).transpose()
-    m, n = shape(dataMat)
+        kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArr[i]): errorCount += 1
+    print "the training error rate is: %f" % (float(errorCount)/m)
+    dataArr,labelArr = loadDataSet('testSetRBF2.txt')
+    errorCount = 0
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    m,n = shape(datMat)
     for i in range(m):
-        kernelEval = kernelTrans(sVs, dataMat[i, :], ('rbf', k1))
-        predict = kernelEval.T * multiply(labelSV, alphas[svInd] + b)
-        if sign(predict) != sign(labelArr[i]): errCount += 1
-    print "the training error rate is: %f" % (float(errCount)/m)
+        kernelEval = kernelTrans(sVs,datMat[i,:],('rbf', k1))
+        predict=kernelEval.T * multiply(labelSV,alphas[svInd]) + b
+        if sign(predict)!=sign(labelArr[i]): errorCount += 1
+    print "the test error rate is: %f" % (float(errorCount)/m)
 
 testRbf()
-
-# dataArr, labelArr = loadDataSet('testSet.txt')
-# b, alphas = smoP(dataArr, labelArr, 0.6, 0.001, 40)
-# print b, alphas
-# w = calcWs(alphas, dataArr, labelArr)
-# print w
